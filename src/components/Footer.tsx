aaf63335b4d33a4349d6a51d5aa1b55dffbed126
Nexi Launch <alrak.solutions@gmail.com>
2024-10-26 19:32:58 +0800

    Add credit for app UI design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -59,7 +59,8 @@ const Footer: React.FC = () => {
             </div>
             <div className="mt-8 md:text-center text-foreground-accent px-6">
                 <p>Copyright &copy; {new Date().getFullYear()} {siteDetails.siteName}. All rights reserved.</p>
-                <p className="text-sm mt-2 text-gray-500">Made with &hearts; by <a href="https://nexilaunch.com">Nexi Launch</a></p>
+                <p className="text-sm mt-2 text-gray-500">Made with &hearts; by <a href="https://nexilaunch.com" target="_blank">Nexi Launch</a></p>
+                <p className="text-sm mt-2 text-gray-500">UI kit by <a href="https://ui8.net/youthmind/products/fintech-finance-mobile-app-ui-kit" target="_blank">Youthmind</a></p>
             </div>
         </footer>
     );
